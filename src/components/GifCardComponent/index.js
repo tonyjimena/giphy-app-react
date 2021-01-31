ca0129fb0e13cac9cbc1a6cbe56eee8277e7a8d6
tonyjimena <jimenaalvarez-tony@gmail.com>
2021-01-31 14:28:32 +0100

    Likes front

--- src/components/GifCardComponent/index.js
+++ src/components/GifCardComponent/index.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./styles.scss";
+import { useState } from "react";
+import Like from "../../assets/img/like-icon.png"
 
 export default function GifCardComponent({ id, title, imageUrl }) {
+  const [like, setLike] = useState(false)
+  const [clase, setClass] = useState("nolike")
+
+  useEffect(() => {
+    like ? setClass("liked") : setClass("nolike")
+  }, [like])
+
   return (
     <div key={id} className="CardComponent">
+      <div className="like" onClick={()=> setLike(!like)}>
+        <img src={Like} width="30px" className={clase}/>
+      </div>
       <h3>{title}</h3>
       <img src={imageUrl} alt={title} className="CardComponent-Image" />
     </div>
